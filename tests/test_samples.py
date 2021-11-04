08a74acb3779027137e6b4fbfa21db48e0dc63d7
James Westman <james@jwestman.net>
2021-11-03 23:16:14 -0500

    ci: Generate test coverage report

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -103,8 +103,6 @@ class TestSamples(unittest.TestCase):
 
 
     def test_sample_errors(self):
-        self.assert_sample_error("conflicting_namespaces")
         self.assert_sample_error("duplicate_obj_id")
         self.assert_sample_error("two_templates")
-        self.assert_sample_error("using_gtk_3")
         self.assert_sample_error("using_invalid_namespace")
