de41f1742a4bd0c7b84815019ddf36b67dc6c5b0
James Coglan <james@neighbourhood.ie>
2025-08-19 13:40:41 +0100

    Add methods to LinterRule for checking for user-facing text

--- blueprintcompiler/linter_rules/utils.py
+++ blueprintcompiler/linter_rules/utils.py
@@ -1,3 +1,21 @@
+from blueprintcompiler.annotations import get_annotation_elements
+from blueprintcompiler.language.values import Translated, Literal, QuotedLiteral
+
+UI_STRING_PROPERTIES = get_annotation_elements()
+
 class LinterRule:
     def __init__(self, problems):
         self.problems = problems
+
+    def is_ui_string(self, type, property):
+        for (m_type, m_name) in UI_STRING_PROPERTIES:
+            if (m_type == type or m_type is None) and m_name == property.name:
+                return True
+        return False
+
+    def get_string_value(self, property):
+        value = property.children[0].child
+        if isinstance(value, Translated):
+            return (value.string, value.range)
+        elif isinstance(value, Literal) and isinstance(value.value, QuotedLiteral):
+            return (value.value.value, value.range)
